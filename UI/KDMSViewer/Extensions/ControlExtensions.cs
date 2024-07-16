db5b3bc55c1b11109d38e735dae61fd308aede8f
김대한 <eogks1792@naver.com>
2024-07-16 10:01:07 +0900

    정리

---
+++ UI/KDMSViewer/Extensions/ControlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace KDMSViewer.Extensions
+{
+    public static class ControlExtensions
+    {
+        public static void ResizeRedraw(this Control control, bool enabled)
+        {
+            var prop = control.GetType().GetProperty("ResizeRedraw", BindingFlags.Instance | BindingFlags.NonPublic);
+            prop.SetValue(control, enabled, null);
+        }
+
+        public static void DoubleBuffered(this Control control, bool enabled)
+        {
+            var prop = control.GetType().GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+            prop.SetValue(control, enabled, null);
+        }
+    }
+}
